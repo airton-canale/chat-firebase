fb0622392daacde397b769490e7fd58679189807
Airton <airtonluizcanalefilho@hotmail.com>
2023-02-21 22:43:10 -0300

    feat: add default and message component

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,17 +1,19 @@
 import React from "react";
-import ChatHeader from "../ChatHeader"
+import ChatHeader from "../ChatHeader";
 import * as C from "./styles";
-import ChatBody from "../ChatBody"
-import ChatFooter from "../ChatFooter"
+import Default from "./../Default";
+import ChatBody from "../ChatBody";
+import ChatFooter from "../ChatFooter";
 
 const Chat = ({ userChat }) => {
-    return (
-        <C.Container>
-            <ChatHeader photoURL={userChat?.photoURL} name={userChat?.name} />
-            <ChatBody chatId={userChat?.chatId} />
-            <ChatFooter chatId={userChat?.chatId} />
-        </C.Container>
-    )
-}
+  if (!userChat) return <Default />;
+  return (
+    <C.Container>
+      <ChatHeader photoURL={userChat?.photoURL} name={userChat?.name} />
+      <ChatBody chatId={userChat?.chatId} />
+      <ChatFooter chatId={userChat?.chatId} />
+    </C.Container>
+  );
+};
 
 export default Chat;
--- src/components/ChatBody/styles.js
+++ src/components/ChatBody/styles.js
@@ -1,8 +1,17 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const Container = styled.div`
-display: flex;
-height: 100vh;
-align-items: center;
-justify-content: center;
-`
+  flex: 1;
+  background-color: #efeae2;
+  overflow-y: auto;
+
+  &::-webkit-scrollbar {
+    width: 6px;
+    border-radius: 10px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
